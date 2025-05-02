a75dfd44486264eb481f77e3311e7ca76d8d5448
Kushan Gunarathna <kushan@enadoc.com>
2025-05-02 16:21:21 +0530

    remove auth for products

--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -5,6 +5,7 @@ const authorizeRoles = require('../middleware/role.middleware');
 const router = express.Router();
 
 // router.get('/', verifyToken,authorizeRoles("superadmin"),getAllProducts)
+router.get('/',getAllProducts)
 router.post('/',createProduct)
 router.get('/:id',getProductById)
 router.put('/:id',updateProduct)
