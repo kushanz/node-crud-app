fd73dc9efbbe6e1fdb2bd68adafb6e7567062297
Kushan Gunarathna <kushan@enadoc.com>
2025-05-21 09:44:40 +0530

    change login response object

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -34,7 +34,16 @@ const login = async (req, res) => {
       return res.status(401).json({ message: 'Invalid email or password' });
     }
     const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1h' });
-    res.status(200).json({ user,token });
+    res.status(200).json({
+      message: `Login successful user ${user.email}`,
+      loggedUser: {
+        token: token,
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+     });
 
   } catch (error) {
     console.error(error);
