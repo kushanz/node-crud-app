df55843c5339ec159787b0ee80507b671f0299a7
Kushan Gunarathna <kushan@enadoc.com>
2025-06-02 12:50:55 +0530

    enable cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv').config();
+const cors = require('cors');
 const app = express();
 app.use(express.json());
+app.use(cors()); // Enable CORS for all routes
 const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: false })); // for parsing application/x-www-form-urlencoded
 
