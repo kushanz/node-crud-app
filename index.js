c8e7b646cb4fccc74c8f90612bb3c6d074fe4188
Kushan Gunarathna <kushan@enadoc.com>
2026-02-05 11:28:27 +0530

    angular port sert cors

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const corsOptions = {
         'http://localhost:8080',
         'http://127.0.0.1:3000',
         'http://127.0.0.1:5173',
-        'http://127.0.0.1:8080'
+        'http://127.0.0.1:4200'  // angular default
       ];
       
       if (allowedOrigins.indexOf(origin) !== -1) {
